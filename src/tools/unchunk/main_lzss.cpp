d3604f3ea5f9e5282936da39f6d6164cda13f93b
CHC NiZ <chcniz@gmail.com>
2025-01-25 13:36:52 -0500

    added thaw360 support, chunk lzss support

---
+++ src/tools/unchunk/main_lzss.cpp
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <cassert>
+#include <FileStream.h>
+#include <stdio.h>
+#include <lzss.h>
+
+int main(int argc, const char* argv[]) {
+	if (argc < 3) {
+		fprintf(stderr, "usage: %s [in] [out]\n", argv[0]);
+		return -1;
+	}
+
+	FILE* fd = fopen(argv[1], "rb");
+	if (fd == NULL) {
+		fprintf(stderr, "Failed to open file %s\n", argv[1]);
+		return -1;
+	}
+
+	FILE* out_fd = fopen(argv[2], "wb");
+	if (out_fd == NULL) {
+		fprintf(stderr, "Failed to open file %s\n", argv[2]);
+		return -1;
+	}
+
+
+	fseek(fd, 0, SEEK_END);
+	long size = ftell(fd);
+	fseek(fd, 0, SEEK_SET);
+
+	char* in_buffer = new char[size];
+	int len = fread(in_buffer, 1, size, fd);
+	if (len != 1) {
+		fprintf(stderr, "Failed to read input buffer");
+		return -1;
+	}
+	fclose(fd);
+
+	char* out = new char[size * 10];
+	len = decompress_lzss((unsigned char *)in_buffer, size, (unsigned char*)out);
+	
+	len = fwrite(out, 1, len, out_fd);
+	if (len != 1) {
+		fprintf(stderr, "Failed to write output buffer");
+		return -1;
+	}
+
+	fclose(fd);
+	fclose(out_fd);
+	delete[] out;
+	delete[] in_buffer;
+	return 0;
+}
